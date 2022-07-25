85aa3724c89a67aa10950e5fcb33950606142568
Vitu12345 <vmafeni@agilesystems.pa.mw>
2022-07-25 06:20:00 +0200

    home page staff >> projects, news, carousel from db

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -289,7 +289,7 @@
 
     <!-- OUR PARTNERS -->
     <div class="section" style="background-color: #cccccc">
-        <div class="">
+        <div class=" pb-5">
             <div class="container">
                 <div class="row">
 
@@ -302,26 +302,30 @@
                     </div>
 
                 </div>
-                <div class="row gutter-5">
-                    <div class="col-6 col-md-2">
-                        <a href="#"><img src="images/dummy-img-200x125.jpg" alt="" class="img-fluid img-border"></a>
-                    </div>
-                    <div class="col-6 col-md-2">
-                        <a href="#"><img src="images/dummy-img-200x125.jpg" alt="" class="img-fluid img-border"></a>
-                    </div>
-                    <div class="col-6 col-md-2">
-                        <a href="#"><img src="images/dummy-img-200x125.jpg" alt="" class="img-fluid img-border"></a>
-                    </div>
-                    <div class="col-6 col-md-2">
-                        <a href="#"><img src="images/dummy-img-200x125.jpg" alt="" class="img-fluid img-border"></a>
-                    </div>
-                    <div class="col-6 col-md-2">
-                        <a href="#"><img src="images/dummy-img-200x125.jpg" alt="" class="img-fluid img-border"></a>
-                    </div>
-                    <div class="col-6 col-md-2">
-                        <a href="#"><img src="images/dummy-img-200x125.jpg" alt="" class="img-fluid img-border"></a>
-                    </div>
 
+                <div class="container">
+                    <section class="customer-logos slider">
+                        <div class="slide"><img
+                                src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg">
+                        </div>
+                        <div class="slide"><img
+                                src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
+                        <div class="slide"><img
+                                src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
+                        <div class="slide"><img
+                                src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg">
+                        </div>
+                        <div class="slide"><img
+                                src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg">
+                        </div>
+                        <div class="slide"><img
+                                src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg">
+                        </div>
+                        <div class="slide"><img
+                                src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg">
+                        </div>
+
+                    </section>
                 </div>
             </div>
         </div>
