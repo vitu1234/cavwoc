83be9b2e57cfd03187b179acd8b1cfe305b7ade6
Vitu12345 <vmafeni@agilesystems.pa.mw>
2022-07-30 20:48:50 +0200

    users, projects,news,partners,others_say,gallery

--- resources/views/admin/incl/sidebar.blade.php
+++ resources/views/admin/incl/sidebar.blade.php
@@ -9,7 +9,7 @@
             <ul id="sidebarnav">
                 <!-- User Profile-->
                 <li class="sidebar-item pt-2">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="dashboard.html"
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/admin"
                        aria-expanded="false">
                         <i class="far fa-clock" aria-hidden="true"></i>
                         <span class="hide-menu">Dashboard</span>
@@ -63,46 +63,21 @@
                     </a>
                 </li>
 
+
                 <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="basic-table.html"
-                       aria-expanded="false">
-                        <i class="fa fa-table" aria-hidden="true"></i>
-                        <span class="hide-menu">Basic Table</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="fontawesome.html"
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="admin/vacancies"
                        aria-expanded="false">
                         <i class="fa fa-font" aria-hidden="true"></i>
-                        <span class="hide-menu">Icon</span>
+                        <span class="hide-menu">Vacancies</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="map-google.html"
-                       aria-expanded="false">
-                        <i class="fa fa-globe" aria-hidden="true"></i>
-                        <span class="hide-menu">Google Map</span>
-                    </a>
+
+                <li class="text-center  ">
+                    <a href="/admin/logout"
+                       class="btn d-grid btn-danger text-white">
+                        <i class="fa fa-power-off" aria-hidden="true"></i>Logout</a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="blank.html"
-                       aria-expanded="false">
-                        <i class="fa fa-columns" aria-hidden="true"></i>
-                        <span class="hide-menu">Blank Page</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="404.html"
-                       aria-expanded="false">
-                        <i class="fa fa-info-circle" aria-hidden="true"></i>
-                        <span class="hide-menu">Error 404</span>
-                    </a>
-                </li>
-                <li class="text-center p-20 upgrade-btn">
-                    <a href="https://www.wrappixel.com/templates/ampleadmin/"
-                       class="btn d-grid btn-danger text-white" target="_blank">
-                        Upgrade to Pro</a>
-                </li>
+
             </ul>
 
         </nav>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'CAVWOC') }}</title>
+
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('admin_assets/plugins/images/favicon.png')}}">
+    <!-- Custom CSS -->
+    <link href="{{asset('admin_assets/plugins/bower_components/chartist/dist/chartist.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet"
+          href="{{asset('admin_assets/plugins/bower_components/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.css')}}">
+    <!-- Custom CSS -->
+    <link href="{{asset('admin_assets/css/style.min.css')}}" rel="stylesheet">
+
+</head>
+
+<body>
+<div class="main-wrapper">
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Login box.scss -->
+    <!-- ============================================================== -->
+    <div class="error-box">
+        <div class="error-body text-center">
+            <h1 class="error-title text-danger">404</h1>
+            <h3 class="text-uppercase error-subtitle">PAGE NOT FOUND !</h3>
+            <p class="text-muted mt-4 mb-4">YOU SEEM TO BE TRYING TO FIND HIS WAY HOME</p>
+            <a href="javascript:history.back()" class="btn btn-danger btn-rounded waves-effect waves-light mb-5 text-white">Go Back </a>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Login box.scss -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Page wrapper scss in scafholding.scss -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Page wrapper scss in scafholding.scss -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Right Sidebar -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Right Sidebar -->
+    <!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- All Required js -->
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+<script src="{{asset('admin_assets/plugins/bower_components/jquery/dist/jquery.min.js')}}"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="{{asset('admin_assets/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('admin_assets/js/app-style-switcher.js')}}"></script>
+<script src="{{asset('admin_assets/plugins/bower_components/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
+<!--Wave Effects -->
+<script src="{{asset('admin_assets/js/waves.js')}}"></script>
+<!--Menu sidebar -->
+<script src="{{asset('admin_assets/js/sidebarmenu.js')}}"></script>
+<!--Custom JavaScript -->
+<script src="{{asset('admin_assets/js/custom.js')}}"></script>
+<!--This page JavaScript -->
+<!--chartis chart-->
+<script src="{{asset('admin_assets/plugins/bower_components/chartist/dist/chartist.min.js')}}"></script>
+<script
+    src="{{asset('admin_assets/plugins/bower_components/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js')}}"></script>
+<script src="{{asset('admin_assets/js/pages/dashboards/dashboard1.js')}}"></script>
+
+</body>
+
+</html>
