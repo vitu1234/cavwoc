104d418034f9dc51f7f41df17fe287ad3e714a82
Vitu12345 <vmafeni@agilesystems.pa.mw>
2022-08-07 08:16:37 +0200

    public -news -gallery -staff -added wysiwyg -carousel -datatable

--- resources/views/public/projects/index.blade.php
+++ resources/views/public/projects/index.blade.php
@@ -14,37 +14,50 @@
                 <div class="row">
 
                     @if(count($projects) > 0)
-                        @foreach($projects as $project)
-                            <div class="col-sm-4 col-md-4">
-                                <div class="box-fundraising" style="height: 400px !important; overflow: hidden;">
-                                    <a href="/projects/{{$project->id}}">
-                                        <div class="media">
-                                            <img style="height: 200px !important; width: 100%"
-                                                 src="{{url('storage/projects/'.$project->img_url)}}" alt="">
-                                        </div>
-                                        <div class="body-content">
-                                            <div style="height: 80px !important; margin-top: 5px;">
-                                                <p class="title"><a
-                                                        href="/projects/{{$project->id}}">{{ \Illuminate\Support\Str::of($project->project_name)->words(20,'...')}}</a>
-                                                </p>
-                                                <span class="text-warning mt-3 mb-3 "><span
-                                                        class="fa fa-clock-o"></span> {{$project->project_type}}</span>
-                                                <div class="text text-dark text-justify">
-                                                    {{ \Illuminate\Support\Str::of($project->project_summary)->words(15,'...')}}
+                        <table style="width:100%" class="table table-borderless" id="news_table">
+                            <tr>
+                                <td>
+                                    <div class="row" style="justify-content: center">
+                                        @foreach($projects as $project)
+                                            <div class="col-sm-3 col-md-3">
+                                                <div class="box-fundraising"
+                                                     style="height: 400px !important; overflow: hidden;">
+                                                    <a href="/projects/{{$project->id}}">
+                                                        <div class="media">
+                                                            <img style="height: 200px !important; width: 100%"
+                                                                 src="{{url('storage/projects/'.$project->img_url)}}"
+                                                                 alt="">
+                                                        </div>
+                                                        <div class="body-content">
+                                                            <div style="height: 80px !important; margin-top: 5px;">
+                                                                <p class="title"><a
+                                                                        href="/projects/{{$project->id}}">{{ \Illuminate\Support\Str::of($project->project_name)->words(8,'...')}}</a>
+                                                                </p>
+                                                                <span class="text-warning mt-3 mb-3 "><span
+                                                                        class="fa fa-clock-o"></span> {{$project->project_type}}</span>
+                                                                <div class="text text-dark text-justify">
+                                                                    {{ \Illuminate\Support\Str::of($project->project_summary)->words(12,'...')}}
+                                                                </div>
+                                                            </div>
+                                                            <div class="progress-fundraising mt-5">
+                                                                <div class="total">
+                                                                    <small>${{number_format($project->amount_raised,2)}}
+                                                                        /${{number_format($project->budgeted_amount,2)}}</small>
+                                                                </div>
+
+                                                                <a href="/projects/{{$project->id}}"
+                                                                   class="btn btn-sm btn-primary text-light"
+                                                                   style="float: right;">Read More</a>
+                                                            </div>
+                                                        </div>
+                                                    </a>
                                                 </div>
                                             </div>
-                                            <div class="progress-fundraising mt-5">
-                                                <div class="total"><small>${{number_format($project->amount_raised,2)}}
-                                                        /${{number_format($project->budgeted_amount,2)}}</small></div>
-
-                                                <a href="/projects/{{$project->id}}" class="btn btn-sm btn-primary text-light"
-                                                   style="float: right;">Read More</a>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        @endforeach
+                                        @endforeach
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
                         <div class="col-sm-12 col-md-12">
                             <div class="spacer-40"></div>
 
